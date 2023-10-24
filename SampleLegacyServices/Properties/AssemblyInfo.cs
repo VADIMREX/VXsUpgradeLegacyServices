16671c54c662eb7fa0f4ac0685e16e6a57d7ed7d
Вадим <nerzhularthas@gmail.com>
2023-10-24 21:39:17 +0600

    Sample legacy project

---
+++ SampleLegacyServices/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LegacyServices")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LegacyServices")]
+[assembly: AssemblyCopyright("Copyright @ 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e6f492f4-4fe2-4855-86f6-f118cb4bbe76")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
