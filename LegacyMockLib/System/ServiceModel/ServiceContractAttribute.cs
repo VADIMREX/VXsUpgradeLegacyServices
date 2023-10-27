e9c27a2d15dd7e8c5b01ad61d1c76fa46ae99150
Вадим <nerzhularthas@gmail.com>
2023-10-27 21:36:49 +0600

    Fixed ServiceContractAttribute logic

--- LegacyMockLib/System/ServiceModel/ServiceContractAttribute.cs
+++ LegacyMockLib/System/ServiceModel/ServiceContractAttribute.cs
@@ -10,6 +10,6 @@ public sealed class ServiceContractAttribute : Attribute
     public Type CallbackContract { get; set; }
     public string ConfigurationName { get; set; }
     public string Name { get; set; }
-    public string Namespace { get; set; } = "http://tempuri.org";
+    public string Namespace { get; set; }
     public SessionMode SessionMode { get; set; }
 }
